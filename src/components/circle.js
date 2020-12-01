b94b083f53745be1a2ad14bc39b3403a8cd3adb2
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-12-01 22:30:15 +0530

    f d

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
+let Shape = require("./shape");
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "blue";
+  }
+  calculateArea() {
+    console.log("calculate");
+  }
+}
+
+module.exports = Circle;
 
 // declare class
-
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
-// declare class
+import { Component } from "react";
 
+// declare class
 // export class using module.exports
+class Shape extends Component {
+  constructor(color) {
+    super();
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("draw a shape");
+  }
+
+  calculateArea() {
+    console.log("calculate the area ");
+  }
+}
+
+module.exports = Shape;
